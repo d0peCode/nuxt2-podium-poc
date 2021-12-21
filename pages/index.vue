9a8a7d061113f1fbb5bfa9d07425d780dd74a127
Tomasz Adamowicz <tomasz@bracketspace.com>
2021-12-21 12:46:36 +0100

    wroking podlet

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <Tutorial/>
+  <div>
+    test - {{ response }}
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  data() {
+    return {
+      response: '',
+    }
+  },
+
+  created() {
+    console.log('created');
+
+    axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      .then(data => {
+        console.log(data);
+
+        this.response = data;
+      })
+      .catch(e => {
+        console.log(e);
+      })
+
+  }
 }
 </script>
