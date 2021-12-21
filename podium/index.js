67fcc2e9a63941c776c2104dd1bfe36d59075d0d
Borys Tyminski <tyminski.borys@gmail.com>
2021-12-21 17:11:31 +0100

    add logs

--- podium/index.js
+++ podium/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const Layout = require('@podium/layout');
+
 const layout = new Layout({
   name: 'myLayout',
   pathname: '/',
 });
+
 const podletA = layout.client.register({
   name: 'myPodletA',
   uri: 'http://localhost:3000/manifest.json',
 });
+
 const app = express();
+
 app.use(layout.middleware());
+
 app.get(layout.pathname(), async (req, res, next) => {
   const incoming = res.locals.podium;
   const [a] = await Promise.all([
@@ -20,4 +25,6 @@ app.get(layout.pathname(), async (req, res, next) => {
         <section>${a.content}</section>
     `);
 });
+
 app.listen(7000);
+console.log('podium js app listening on port 7000')
