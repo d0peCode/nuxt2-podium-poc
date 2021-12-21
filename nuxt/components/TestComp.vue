912a5baac49d2c4a67efa2ca36007df25db00109
Borys Tyminski <tyminski.borys@gmail.com>
2021-12-21 16:32:02 +0100

    full demo

---
+++ nuxt/components/TestComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>testcomp.vue</h1>
+    <button @click="test">test1</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    test() {
+      console.log('test')
+    }
+  }
+}
+</script>
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="test">test</button>
+    data from api:
+    <pre>
+      {{ mountains }}
+    </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+
+  data() {
+    return {
+      mountains: []
+    }
+  },
+
+  mounted() {
+    console.log('mounted')
+  },
+
+  async fetch() {
+    this.mountains = await fetch(
+      'https://api.nuxtjs.dev/mountains'
+    ).then(res => res.json())
+  },
+
+  methods: {
+    test() {
+      console.log('test')
+    }
+  }
+}
+</script>
