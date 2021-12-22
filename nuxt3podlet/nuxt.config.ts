8d06e1bda6c5fcf5cd61f719378152b4ef08e61b
Borys Tyminski <tyminski.borys@gmail.com>
2021-12-22 15:20:21 +0100

    wip

--- nuxt3podlet/nuxt.config.ts
+++ nuxt3podlet/nuxt.config.ts
@@ -2,5 +2,18 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  modules: [
+    [
+      'nuxt-podium-module',
+      {
+        podletOptions: {
+          name: 'myPodletNameb', // required
+          version: `1.0.0`, // required
+          pathname: '/', // required
+          manifest: '/manifest.json', // optional
+          development: false // optional, defaults to false
+        }
+      }
+    ]
+  ]
 })
